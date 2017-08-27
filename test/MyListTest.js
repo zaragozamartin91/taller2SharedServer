9b0ea52ebb02caaa6ce8e41559aea18bd5b0cc1b
zaragozamartin91 <zaragozamartin91@gmail.com>
2017-08-27 15:28:43 -0300

    Se configuro la generacion de reportes de coverage de codigo usando istambul y mocha

---
+++ test/MyListTest.js
@@ -0,0 +1,16 @@
+var MyList = require("../utils/MyList");
+var assert = require('assert');
+
+describe('MyList', function () {
+    beforeEach(function () {
+        console.log("before each!");
+    });
+
+    describe('#add(item)', function () {
+        it('should add an item', function () {
+            var list = new MyList();
+            list.add(28);
+            assert.equal(28, list.items[0]);
+        });
+    });
+});
---
+++ utils/MyList.js
@@ -0,0 +1,9 @@
+function MyList() {
+    this.items = []
+}
+
+MyList.prototype.add = function(item) {
+    this.items.push(item);
+};
+
+module.exports = MyList;
