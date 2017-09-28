ea493b31ada49a2e55359ffa9e54294371438442
Zaragoza <martin.zaragoza@accusys.com.ar>
2017-09-28 15:19:07 -0300

    Draft login backoffice
    Se termino con un draft del login del backoffice para la administracion
    del shared server.

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -9,49 +8,58 @@ import FlatButton from 'material-ui/FlatButton';
 
 import Header from './Header';
 
+/* FIN DE IMPORTS -------------------------------------------------------------------------------------- */
 
-/* ESTE FRAGMENTO DE CODIGO ES REQUERIDO PARA LOS EVENTOS DE TIPO TOUCH O CLICK EN COMPONENTES MATERIAL-UI */
-const injectTapEventPlugin = require('react-tap-event-plugin');
-injectTapEventPlugin();
-/* -------------------------------------------------------------------------------------------------------- */
-
-let messageDiv = document.getElementById('message');
-let message = messageDiv ? messageDiv.getAttribute('data-value') : null;
+const EMPTY_CALLBACK = () => { };
 
 const Login = React.createClass({
+    getInitialState: function () {
+        return { username: '', password: '' };
+    },
+
+    getDefaultProps: function () {
+        return { onSubmit: EMPTY_CALLBACK };
+    },
+
     submitForm: function () {
-        console.log('Submiting form');
-        if (this.form) this.form.submit();
+        const user = { username: this.state.username, password: this.state.password };
+        console.log('Subiendo:');
+        console.log(user);
+        this.props.onSubmit(user);
+    },
+
+    handleKeyPress: function (event) {
+        if (event.key == 'Enter') this.submitForm();
     },
 
     render: function () {
-        let msgElem = message ? <p style={{ color: 'red' }}>{message}</p> : <div />;
-
         return (
-            <div>
+            <div onKeyPress={this.handleKeyPress}>
                 <Header title="Shared server" />
-                {msgElem}
-                
+
                 <MuiThemeProvider>
                     <Card>
                         <CardHeader
                             title="Iniciar sesion"
-                            subtitle="Usa una cuenta existente." />
+                            subtitle="Usuario de negocio" />
 
                         <CardText expandable={false}>
-                            <form method="POST" action="/login" ref={f => { this.form = f; }}>
-                                <TextField
-                                    name="username"
-                                    hint="username"
-                                    floatingLabelText="username" /><br />
+                            <TextField
+                                name="username"
+                                hint="username"
+                                floatingLabelText="username"
+                                value={this.state.username}
+                                onChange={e => this.setState({ username: e.target.value })} /><br />
 
-                                <TextField
-                                    name="password"
-                                    hintText="Password"
-                                    floatingLabelText="Password"
-                                    type="password" /><br />
-                            </form>
+                            <TextField
+                                name="password"
+                                hintText="Password"
+                                floatingLabelText="Password"
+                                type="password"
+                                value={this.state.password}
+                                onChange={e => this.setState({ password: e.target.value })} /><br />
                         </CardText>
+
                         <CardActions>
                             <FlatButton label="Iniciar sesion" onClick={this.submitForm} />
                         </CardActions>
@@ -63,7 +71,4 @@ const Login = React.createClass({
     }
 });
 
-ReactDom.render(
-    <Login />,
-    document.getElementById('root')
-);
+export default Login;
--- components/MainApp.js
+++ components/MainApp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
@@ -13,9 +13,10 @@ import axios from 'axios';
 
 import Index from './Index';
 import FormExample from './FormExample';
+import Login from './Login';
 
 /* ESTE FRAGMENTO DE CODIGO ES REQUERIDO PARA LOS EVENTOS DE TIPO TOUCH O CLICK EN COMPONENTES MATERIAL-UI */
-const injectTapEventPlugin = require('react-tap-event-plugin');
+import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 /* -------------------------------------------------------------------------------------------------------- */
 
@@ -23,6 +24,7 @@ const MainApp = React.createClass({
     getInitialState: function () {
         return {
             drawerOpen: false,
+            user: null
         };
     },
 
@@ -44,32 +46,43 @@ const MainApp = React.createClass({
         this.setState({ drawerOpen: false });
     },
 
+    setUser: function (user) {
+        this.setState({ user });
+    },
+
     render: function () {
         console.log('RENDERING MainApp!');
 
-        return (
-            <MuiThemeProvider>
-                <div>
-                    <AppBar
-                        onLeftIconButtonTouchTap={this.toggleDrawer}
-                        title="Shared server" />
+        const user = this.state.user;
+        if (user) {
+            /* Si un usuario inicio sesion, renderizo la app normal */
+            return (
+                <MuiThemeProvider>
+                    <div>
+                        <AppBar
+                            onLeftIconButtonTouchTap={this.toggleDrawer}
+                            title="Shared server" />
 
-                    <Drawer open={this.state.drawerOpen} docked={false} onRequestChange={this.onDrawerRequestChange} >
-                        <Link to="/Index" onClick={this.closeDrawer}><MenuItem >Principal</MenuItem></Link>
-                        <Link to="/FormExample" onClick={this.closeDrawer}><MenuItem >FormExample</MenuItem></Link>
-                        <MenuItem primaryText='Usuarios'
-                            rightIcon={<ArrowDropRight />}
-                            menuItems={[
-                                <Link to="/Users/Create" onClick={this.closeDrawer}><MenuItem >Crear</MenuItem></Link>
-                            ]}
-                        />
-                    </Drawer>
+                        <Drawer open={this.state.drawerOpen} docked={false} onRequestChange={this.onDrawerRequestChange} >
+                            <Link to="/Index" onClick={this.closeDrawer}><MenuItem >Principal</MenuItem></Link>
+                            <Link to="/FormExample" onClick={this.closeDrawer}><MenuItem >FormExample</MenuItem></Link>
+                            <MenuItem primaryText='Usuarios'
+                                rightIcon={<ArrowDropRight />}
+                                menuItems={[
+                                    <Link to="/Users/Create" onClick={this.closeDrawer}><MenuItem >Crear</MenuItem></Link>
+                                ]}
+                            />
+                        </Drawer>
 
-                    <Route path="/Index" component={Index} />
-                    <Route path="/FormExample" component={FormExample} />
-                </div>
-            </MuiThemeProvider>
-        );
+                        <Route path="/Index" component={Index} />
+                        <Route path="/FormExample" component={FormExample} />
+                    </div>
+                </MuiThemeProvider>
+            );
+        } else {
+            /* Sino, renderizo la pagina de login */
+            return <Login onSubmit={this.setUser} />;
+        }
     }
 });
 
